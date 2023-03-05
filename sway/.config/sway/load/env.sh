a0353909942f4dc40d33b9827316e1e2466774ec
v1adhope <vladislavgardner@gmail.com>
2023-03-05 18:58:31 +0300

    Some fixes - Update modules

--- sway/.config/sway/load/env.sh
+++ sway/.config/sway/load/env.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 # Steam fix
-export SDL_VIDEODRIVER=x11
+# export SDL_VIDEODRIVER=x11
+# export SDL_DYNAMIC_API=/usr/lib/libSDL2-2.0.so
+
+# Firefox fix
+export MOZ_ENABLE_WAYLAND=1
 
 # Wayland fix
-#export SDL_VIDEODRIVER=wayland
-export _JAVA_AWT_WM_NONREPARENTING=1
-export QT_QPA_PLATFORM=wayland
+export XDG_SESSION_TYPE=wayland
+export QT_WAYLAND_FORCE_DPI=physical
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export QT_QPA_PLATFORMTHEME=gnome
+export SDL_VIDEODRIVER=wayland
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
-export GDK_BACKEND="wayland,x11"
+export _JAVA_AWT_WM_NONREPARENTING=1
+# export QT_QPA_PLATFORM=wayland
+# export GDK_BACKEND="wayland,x11"
+
+# Customize waybar
+export CITY_WEATHER=Penza
